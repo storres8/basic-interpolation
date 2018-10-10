a884cd70a92899d2d1bee05e96579f505e1e32a9
storres8 <stevetorres7562@gmail.com>
2018-10-10 19:38:02 -0400

    worked on interpolation with float formatting

--- string_interpolation.py
+++ string_interpolation.py
@@ -8,5 +8,25 @@ print("hello {1}, this is writtin in {0}".format('python', 'steven'))
 
 # we can also assign the words we want interpolated as values to a variable
 # and this way we don't have to know their index, but rather their variable
-print('helloooo {s} this is writtin in {p}'.format( p='python', s='steven'))
+print('helloooo {s} this is writtin in {p}'.format(p='python', s='steven'))
 #all these methods are valid if we use the .format() function for interpolation
+#also since the variables are pointing to a value we can use that variable
+# as many times as we want
+print('hello {s} this is writtin in {s}'.format(p='python', s='steven'))
+#this might not make sense but it shows that the variables are recyclable
+
+
+# for numbers there is also float formatting that allows us to change
+# how many decimal places we want to round by and the white space before
+# the number.
+
+# Float formatting interpolation: "{value:width.precisionf}"
+result=100/567
+print("The result is {r}".format(r=result))
+# prints The result is 0.1763668430335097
+
+print("and this is the result rounded to 3 decimal places {r:1.3f}".format(r=result))
+# prints and this is the result rounded to 3 decimal places 0.176
+
+# the width value simply increases the space between the number and the word right
+# before it 
